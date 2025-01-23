b0887e9fc3bdf619ca0ac799979df256e5286c7f
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-01-23 14:24:50 +0700

    chore: update multiple_contract test

--- examples/multiple-contracts/lib.rs
+++ examples/multiple-contracts/lib.rs
@@ -1,35 +1,30 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
-mod balance_checker {
+mod checker {
     use ink::{
         env::{
             call::{build_call, ExecutionInput, Selector},
             DefaultEnvironment,
         },
-        H160, U256,
+        H160,
     };
 
     #[ink(storage)]
-    pub struct BalanceChecker {
-        account: H160,
-        token_contract: H160,
+    pub struct Checker {
+        contract: H160,
     }
 
-    impl BalanceChecker {
+    impl Checker {
         #[ink(constructor)]
-        pub fn new(account: H160, token_contract: H160) -> Self {
-            Self {
-                account,
-                token_contract,
-            }
+        pub fn new(contract: H160) -> Self {
+            Self { contract }
         }
 
         #[ink(message)]
         pub fn check(&self) -> bool {
             build_call::<DefaultEnvironment>()
-                .call(self.token_contract)
-                .transferred_value(U256::zero())
+                .call(self.contract)
                 .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
                     "Flipper::get"
                 ))))
@@ -43,32 +38,31 @@ mod balance_checker {
 mod tests {
     use std::error::Error;
 
-    use drink::session::{Session, NO_ARGS, NO_ENDOWMENT, NO_SALT};
+    use drink::session::{Session, NO_ARGS, NO_ENDOWMENT};
 
     #[drink::contract_bundle_provider]
     enum BundleProvider {}
 
     #[drink::test]
     fn contracts_work_correctly(mut session: Session) -> Result<(), Box<dyn Error>> {
-        let token_contract = session.deploy_bundle(
+        let contract = session.deploy_bundle(
             BundleProvider::Flipper.bundle()?,
             "new",
             &["true"],
-            NO_SALT,
+            Some([1; 32]),
             NO_ENDOWMENT,
         )?;
 
-        let user_account = session.get_actor();
         let _checker_contract = session.deploy_bundle(
             BundleProvider::local()?,
             "new",
-            &[user_account.to_string(), token_contract.to_string()],
-            NO_SALT,
+            &[format!("{:?}", contract)],
+            Some([2; 32]),
             NO_ENDOWMENT,
         )?;
 
-        let balance: u128 = session.call("check", NO_ARGS, NO_ENDOWMENT)??;
-        assert_eq!(balance, 10);
+        let value: bool = session.call("check", NO_ARGS, NO_ENDOWMENT)??;
+        assert!(value);
 
         Ok(())
     }
